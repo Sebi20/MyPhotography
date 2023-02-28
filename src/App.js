53e8f59ac7b694bd4da3006d28614c3d931ee5a1
Sebastian <sebastian.stephenson1@gmail.com>
2023-02-27 23:04:48 -0500

    Added online links instead of photos to save space

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <></>
+    <>
+      <img src="https://www.flickr.com/photos/10mix/35555734303/" alt=""></img>
+    </>
   );
 }
 
---
+++ src/photos.js
@@ -0,0 +1,32 @@
+export const photos = [
+    {
+        url: "https://pbs.twimg.com/media/FqBfKjOWIAEL5Rd?format=jpg&name=large",
+        desc: "Shot of the skies during ascent",
+        dateTaken: "01-15-2023"
+    },
+    {
+        url: "https://pbs.twimg.com/media/FqBkj40XsAI6_wZ?format=jpg&name=small",
+        desc: "ARTECHOUSE NYC Exhibit",
+        dateTaken: "02-12-2023"
+    },
+    {
+        url: "https://pbs.twimg.com/media/FqBl3ywX0AE_NTE?format=jpg&name=small",
+        desc: "Sunset over the Caribbean sea",
+        dateTaken: "01-11-2023"
+    },
+    {
+        url: "https://pbs.twimg.com/media/FqBmryrX0AIvwEJ?format=jpg&name=small",
+        desc: "Jenga with a twist",
+        dateTaken: "10-13-2022"
+    },
+    {
+        url: "https://pbs.twimg.com/media/FqBfKjOWIAEL5Rd?format=jpg&name=large",
+        desc: "Lamborghini performante spider in the city",
+        dateTaken: "08-14-2022"
+    },
+    {
+        url: "https://pbs.twimg.com/media/FqBoAiJXsAEFm87?format=jpg&name=small",
+        desc: "First time trying A5 wagyu beef",
+        dateTaken: "08-14-2022"
+    }
+]
